c5394fc04fb7ee3e731c280772086213e5ca998f
Vishal Sohaliya <132877214+vills25@users.noreply.github.com>
2025-11-13 17:36:19 +0530

    #

--- medium_blog_api_project/settings.py
+++ medium_blog_api_project/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-($!x0_-yl-0ub3e4$t94)oofe(x983!1m1_i@80xd7l=redw1k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['medium-blogs-restapi.onrender.com']
 
 # Application definition
 
