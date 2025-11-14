2bcb88caa01c9520c58737891ac1e7cca0c45a28
Vishal Sohaliya <132877214+vills25@users.noreply.github.com>
2025-11-14 11:42:22 +0530

    #

--- medium_blog_api_project/settings.py
+++ medium_blog_api_project/settings.py
@@ -136,7 +136,6 @@ DATABASES = {
     'NAME': 'medium_blog_api_database',
     'USER': 'root',
     'PASSWORD': '',
-    # 'HOST': 'localhost',
     'HOST': '127.0.0.1',
     'PORT': '3306',
     }
@@ -176,7 +175,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
