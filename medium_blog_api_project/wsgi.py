b270086cba8b521757f44ca471b536420bf56955
Vishal Sohaliya <132877214+vills25@users.noreply.github.com>
2025-11-14 11:58:00 +0530

    #

--- medium_blog_api_project/wsgi.py
+++ medium_blog_api_project/wsgi.py
@@ -1,14 +1,26 @@
-"""
-WSGI config for medium_blog_api_project project.
+# """
+# WSGI config for medium_blog_api_project project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+# """
 
+# import os
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medium_blog_api_project.settings')
+# application = get_wsgi_application()
+
+import sys
 import os
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medium_blog_api_project.settings')
+project_home = '/home/vishalmediumblogapi/Medium-Blogs-RestAPI'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'medium_blog_api_project.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
