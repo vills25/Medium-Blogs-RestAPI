b53af9297650e9547a3a55c49903290b842d6a17
Vishal Sohaliya <132877214+vills25@users.noreply.github.com>
2025-11-14 12:35:36 +0530

    #

--- medium_blog_api_project/settings.py
+++ medium_blog_api_project/settings.py
@@ -123,23 +123,23 @@ WSGI_APPLICATION = 'medium_blog_api_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-  'default' : {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'medium_blog_api_database',
-    'USER': 'root',
-    'PASSWORD': '',
-    'HOST': '127.0.0.1',
-    'PORT': '3306',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#   'default' : {
+#     'ENGINE': 'django.db.backends.mysql',
+#     'NAME': 'medium_blog_api_database',
+#     'USER': 'root',
+#     'PASSWORD': '',
+#     'HOST': '127.0.0.1',
+#     'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
